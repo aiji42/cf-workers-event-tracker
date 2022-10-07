a4d56203e77285ba9e889cfaa4e601c96a775e00
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-10-07 23:31:46 +0900

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Hono } from "hono";
+import { Context, Hono } from "hono";
 import type { Database } from "@cloudflare/d1";
 import { D1QB } from "workers-qb";
 import { v4 as uuidv4 } from "uuid";
@@ -37,87 +37,10 @@ app.get("*", async (c) => {
   c.executionCtx.waitUntil(
     (async () => {
       const qb = new D1QB(c.env.DB);
-      try {
-        const ua = UAParser(c.req.headers.get("User-Agent") ?? "");
-        const url = new URL(c.req.url);
-        await qb.insert({
-          tableName: "Visit",
-          data: {
-            visitToken,
-            visitorToken,
-            longitude: c.req.cf?.longitude ? Number(c.req.cf?.longitude) : null,
-            latitude: c.req.cf?.latitude ? Number(c.req.cf?.latitude) : null,
-            continent: c.req.cf?.continent ?? null,
-            country: c.req.cf?.country ?? null,
-            city: c.req.cf?.city ?? null,
-            region: c.req.cf?.region ?? null,
-            regionCode: c.req.cf?.regionCode ?? null,
-            postalCode: c.req.cf?.postalCode ?? null,
-            timezone: c.req.cf?.timezone ?? null,
-            ip: c.req.headers.get("CF-Connecting-IP"),
-            referrer: c.req.headers.get("Referer"),
-            referringDomain: c.req.headers.get("Referer")
-              ? new URL(c.req.headers.get("Referer")!).host
-              : null,
-            landingPage: url.toString(),
-            userAgent: c.req.headers.get("User-Agent"),
-            browser: ua.browser.name ?? null,
-            browserVersion: ua.browser.version ?? null,
-            os: ua.os.name ?? null,
-            osVersion: ua.os.version ?? null,
-            deviceType: ua.device.type ?? null,
-            deviceModel: ua.device.model ?? null,
-            deviceVendor: ua.device.vendor ?? null,
-            utmSource: url.searchParams.get("utm_source"),
-            utmMedium: url.searchParams.get("utm_medium"),
-            utmTerm: url.searchParams.get("utm_term"),
-            utmContent: url.searchParams.get("utm_content"),
-            utmCampaign: url.searchParams.get("utm_campaign"),
-            startedAt: new Date().toISOString(),
-          },
-        });
-      } catch (_) {}
-
-      try {
-        const ua = UAParser(c.req.headers.get("User-Agent") ?? "");
-        const url = new URL(c.req.url);
-        await qb.insert({
-          tableName: "Event",
-          data: {
-            visitToken,
-            key: "pageview",
-            value: "",
-            href: url.href,
-            orign: url.origin,
-            protcol: url.protocol,
-            host: url.host,
-            port: url.port,
-            pathname: url.pathname,
-            search: url.search,
-            longitude: c.req.cf?.longitude ? Number(c.req.cf?.longitude) : null,
-            latitude: c.req.cf?.latitude ? Number(c.req.cf?.latitude) : null,
-            continent: c.req.cf?.continent ?? null,
-            country: c.req.cf?.country ?? null,
-            city: c.req.cf?.city ?? null,
-            region: c.req.cf?.region ?? null,
-            regionCode: c.req.cf?.regionCode ?? null,
-            postalCode: c.req.cf?.postalCode ?? null,
-            timezone: c.req.cf?.timezone ?? null,
-            ip: c.req.headers.get("CF-Connecting-IP"),
-            userAgent: c.req.headers.get("User-Agent"),
-            browser: ua.browser.name ?? null,
-            browserVersion: ua.browser.version ?? null,
-            os: ua.os.name ?? null,
-            osVersion: ua.os.version ?? null,
-            deviceType: ua.device.type ?? null,
-            deviceModel: ua.device.model ?? null,
-            deviceVendor: ua.device.vendor ?? null,
-            time: new Date().toISOString(),
-          },
-        });
-      } catch (e) {
-        console.error(e);
-      }
+      await Promise.all([
+        insertVisit(c, visitToken, visitorToken, qb),
+        insertEvent(c, visitToken, visitorToken, qb),
+      ]);
     })()
   );
 
@@ -133,5 +56,82 @@ app.get("*", async (c) => {
 
   return new Response(res.body, { headers: newHead });
 });
+
+const getMetadata = (c: Context) => {
+  const ua = UAParser(c.req.headers.get("User-Agent") ?? "");
+
+  return {
+    longitude: c.req.cf?.longitude ? Number(c.req.cf?.longitude) : null,
+    latitude: c.req.cf?.latitude ? Number(c.req.cf?.latitude) : null,
+    continent: c.req.cf?.continent ?? null,
+    country: c.req.cf?.country ?? null,
+    city: c.req.cf?.city ?? null,
+    region: c.req.cf?.region ?? null,
+    regionCode: c.req.cf?.regionCode ?? null,
+    postalCode: c.req.cf?.postalCode ?? null,
+    timezone: c.req.cf?.timezone ?? null,
+    ip: c.req.headers.get("CF-Connecting-IP"),
+    userAgent: c.req.headers.get("User-Agent"),
+    browser: ua.browser.name ?? null,
+    browserVersion: ua.browser.version ?? null,
+    os: ua.os.name ?? null,
+    osVersion: ua.os.version ?? null,
+    deviceType: ua.device.type ?? null,
+    deviceModel: ua.device.model ?? null,
+    deviceVendor: ua.device.vendor ?? null,
+  };
+};
+
+const insertVisit = async (c: Context, visitToken: string, visitorToken: string, qb: D1QB) => {
+  const url = new URL(c.req.url);
+
+  try {
+    await qb.insert({
+      tableName: "Visit",
+      data: {
+        visitToken,
+        visitorToken,
+        ...getMetadata(c),
+        referrer: c.req.headers.get("Referer"),
+        referringDomain: c.req.headers.get("Referer")
+          ? new URL(c.req.headers.get("Referer")!).host
+          : null,
+        landingPage: url.toString(),
+        utmSource: url.searchParams.get("utm_source"),
+        utmMedium: url.searchParams.get("utm_medium"),
+        utmTerm: url.searchParams.get("utm_term"),
+        utmContent: url.searchParams.get("utm_content"),
+        utmCampaign: url.searchParams.get("utm_campaign"),
+        startedAt: new Date().toISOString(),
+      },
+    });
+  } catch (_) {}
+};
+
+const insertEvent = async (c: Context, visitToken: string, visitorToken: string, qb: D1QB) => {
+  const url = new URL(c.req.url);
+
+  try {
+    await qb.insert({
+      tableName: "Event",
+      data: {
+        visitToken,
+        key: "pageview",
+        value: "",
+        href: url.href,
+        orign: url.origin,
+        protcol: url.protocol,
+        host: url.host,
+        port: url.port,
+        pathname: url.pathname,
+        search: url.search,
+        ...getMetadata(c),
+        time: new Date().toISOString(),
+      },
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
 
 export default app;
